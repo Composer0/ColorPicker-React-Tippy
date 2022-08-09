778c3fa1c2365ed9b3aa14bced4b88e680888e16
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-08-09 15:18:22 -0400

    Added className="colorBackground" and theme="nice"

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,28 +3,31 @@ import './App.css';
 
 import {CirclePicker} from 'react-color';
 import Tippy from '@tippyjs/react';
-import "tippy.js/animations/perspective.css";
+import 'tippy.js/animations/perspective.css';
+import 'tippy.js/themes/light.css'
 
 
 
 function App() {
-  const [selectedColor, setSelectedColor] = useState('#ccc');
+  const [selectedColor, setSelectedColor] = useState('rebeccapurple');
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ backgroundColor: selectedColor}}>
         <h1 className="header-text">Hello World</h1>
       </header>
       <div className="area" style={{ backgroundColor: selectedColor}}>
 
       </div>
-
-    <Tippy trigger={"click"} interactive={true} animation={"perspective"} placement={'bottom'} content={
+    <Tippy theme="nice" trigger="click" animation={'perspective'} interactive={true} placement={'bottom'} content={
       <CirclePicker
+        className="colorBackground"
         color={selectedColor}
         onChangeComplete={color => setSelectedColor(color.hex)}
+        
       />
     }>
-      <button className='ref-button'>Click to Change Box Color</button>
+      <button  className='ref-button'>Change Header Box Color</button>
+
     </Tippy>
     </div>
   );
